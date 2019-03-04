e47579842a9d1cc4304cf270d47f91bd1ac83227
fab <fabrizio.salmi@gmail.com>
2019-03-04 21:44:51 +0100

    Update wpvg_options.php

--- wpvg_options.php
+++ wpvg_options.php
@@ -1,45 +1,45 @@
 <?php
 // create custom plugin settings menu
-add_action('admin_menu', 'my_cool_plugin_create_menu');
+add_action('admin_menu', 'wpvg_create_menu');
 
-function my_cool_plugin_create_menu() {
+function wpvg_create_menu() {
 
 	//create new top-level menu
-	add_menu_page('My Cool Plugin Settings', 'Cool Settings', 'administrator', __FILE__, 'my_cool_plugin_settings_page' , plugins_url('/images/icon.png', __FILE__) );
+	add_menu_page('WPVG', 'WPVG', 'administrator', __FILE__, 'wpvg_settings_page' , plugins_url('/images/icon.png', __FILE__) );
 
 	//call register settings function
-	add_action( 'admin_init', 'register_my_cool_plugin_settings' );
+	add_action( 'admin_init', 'register_wpvg_settings' );
 }
 
 
-function register_my_cool_plugin_settings() {
+function register_wpvg_settings() {
 	//register our settings
-	register_setting( 'my-cool-plugin-settings-group', 'new_option_name' );
-	register_setting( 'my-cool-plugin-settings-group', 'some_other_option' );
-	register_setting( 'my-cool-plugin-settings-group', 'option_etc' );
+	register_setting( 'wpvg-settings-group', 'new_option_name' );
+	register_setting( 'wpvg-settings-group', 'some_other_option' );
+	register_setting( 'wpvg-settings-group', 'option_etc' );
 }
 
-function my_cool_plugin_settings_page() {
+function wpvg_settings_page() {
 ?>
 <div class="wrap">
-<h1>Your Plugin Name</h1>
+<h1>WPVG</h1>
 
 <form method="post" action="options.php">
-    <?php settings_fields( 'my-cool-plugin-settings-group' ); ?>
-    <?php do_settings_sections( 'my-cool-plugin-settings-group' ); ?>
+    <?php settings_fields( 'wpvg-settings-group' ); ?>
+    <?php do_settings_sections( 'wpvg-settings-group' ); ?>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">New Option Name</th>
+        <th scope="row">Enabled</th>
         <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
         </tr>
          
         <tr valign="top">
-        <th scope="row">Some Other Option</th>
+        <th scope="row">API URL</th>
         <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
         </tr>
         
         <tr valign="top">
-        <th scope="row">Options, Etc.</th>
+        <th scope="row">API KEY</th>
         <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
         </tr>
     </table>
